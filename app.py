88614e51c447d8eb5fd7b5300c6463067d1181bc
Deer5000 <khidrbrinkley32@gmail.com>
2019-10-01 14:42:58 -0700

    Users can see all playlists

--- app.py
+++ app.py
@@ -7,5 +7,19 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is Cool!!')
 
+
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/')
+def playlists_index():
+    """Show all playlists."""
+    # for song in playlists:
+    #     print(song)
+    return render_template('playlists_index.html', playlists=playlists)
+
 if __name__ == '__main__':
     app.run(debug=True)
