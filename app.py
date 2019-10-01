74d52fb6413a739cf0702fec5a2d1cd178ed37ec
Deer5000 <khidrbrinkley32@gmail.com>
2019-10-01 16:43:09 -0700

    Users can create playlists

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 from pymongo import MongoClient
-
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
-
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('home.html', msg='Flask is Cool!!')
-
-
-# OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': 'Exotic Animals Acting Weird', 'description': 'Ocelot biting kid!'},
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
 
 @app.route('/')
 def playlists_index():
@@ -28,17 +11,30 @@ def playlists_index():
     return render_template('playlists_index.html', playlists=playlists.find())
 
 
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
 
-# @app.route('/')
-# def playlists_index():
-#     """Show all playlists."""
-#     return render_template('playlists_index.html', playlists=playlists)
-# @app.route('/')
-# def playlists_index():
-#     """Show all playlists."""
-#      for song in playlists:
-#          print(song)
-#     return render_template('playlists_index.html', playlists=playlists)
+
+# @app.route('/playlists', methods=['POST'])
+# def playlists_submit():
+#     """Submit a new playlist."""
+#     print(request.form.to_dict())
+#     return redirect(url_for('playlists_index'))
+
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
